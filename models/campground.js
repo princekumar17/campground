2a9445fc0858bd2a81d4568514bd387416a65103
Prince Kumar <princekumar17107@gmail.com>
2023-08-12 13:33:17 +0530

    Added review section and flash messages

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose');
+const Review= require('./review')
 const Schema =mongoose.Schema;
 
 const CampgroundSchema=new Schema({
@@ -6,8 +7,25 @@ const CampgroundSchema=new Schema({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+           type: Schema.Types.ObjectId,
+           ref: 'Review' 
+        }
+    ]
 });
+
+//Mongoose Query Middleware .It is gonna pass in doc , find it and delete it
+CampgroundSchema.post('findOneAndDelete',async function(doc){
+    if(doc){
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports=mongoose.model('Campground',CampgroundSchema);
 
